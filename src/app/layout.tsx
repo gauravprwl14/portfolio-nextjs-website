1e9eabbce90447e9aca2f6ebf94ef93a2bc67bfd
Gaurav Porwal <gauravprwl14@gmail.com>
2024-03-24 19:49:22 +0530

    added the basic layout component for the portfolio page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="m-0">
+        {/* <div className="flex flex-1 flex-col w-full h-full clear-both float-left relative"> */}
+        <div className="flex flex-col w-full h-full">
+          <Header></Header>
+          {children}
+        </div>
+        asd
+      </body>
     </html>
   );
 }
