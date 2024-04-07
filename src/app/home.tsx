04d33e0b7c445e294e7104c603c0453ccd58311d
Gaurav Porwal <gauravprwl14@gmail.com>
2024-04-07 14:29:28 +0530

    Add mouse icon to hero section

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -42,25 +42,26 @@ const Home = () => {
 
 const HeroContent = ({}) => {
   return (
-    <div className="flex flex-row h-full mx-auto px-10 max-w-screen-2xl ">
-      <div className="section-1 w-full flex items-center  justify-center">
-        <div className="pr-5 p-2 ">
-          <div className=" text-2xl font-semibold mb-5">
+    <div className="flex flex-col-reverse lg:flex-row lg:justify-normal lg:items-stretch  justify-center items-center  h-full mx-auto px-4 lg:px-10 max-w-screen-2xl ">
+      {/* <div className="flex flex-row h-full mx-auto px-10 max-w-screen-2xl"> */}
+      <div className="section-1 w-full flex items-center justify-center">
+        <div className="pr-5 p-2 flex flex-col items-center lg:items-start ">
+          <div className="hidden lg:flex lg:text-2xl font-semibold mb-5">
             <h3>Welcome to my portfolio</h3>
           </div>
-          <div className=" text-5xl py-2 font-semibold">
+          <div className="text-2xl  lg:text-5xl py-2 font-semibold">
             <h3 className="">{`Hello, I'm`}</h3>
           </div>
 
-          <div className="text-6xl -ml-1 py-2 text-blue-600 font-bold font-jost mb-3">
+          <div className="text-4xl lg:text-6xl -ml-1 py-2 text-blue-600 font-bold font-jost lg:mb-3">
             <h3>{USER_OBJ.name}</h3>
           </div>
 
-          <div className="text-5xl py-2 font-semibold">
+          <div className="text-2xl lg:text-5xl py-2 font-semibold">
             <h3>{USER_OBJ.designation}</h3>
           </div>
 
-          <div className="designation text-2xl my-5 text-gray-700 font-jost">
+          <div className="designation lg:text-2xl my-5 text-gray-700 font-jost text-center lg:text-start">
             <h3>{USER_OBJ.description}</h3>
           </div>
 
@@ -85,10 +86,16 @@ const HeroContent = ({}) => {
               })}
             </div>
           </div>
+
+          <div className="mouse-container absolute bottom-4 w-full left-2/4">
+            <h3 className="">
+              <img src={"/img/slider/mouse.svg"} alt="image" />
+            </h3>
+          </div>
         </div>
       </div>
 
-      <div className="section-2 w-full flex items-center flex-col justify-center">
+      <div className="section-2 w-60 lg:mb-10 lg:h-full lg:w-full flex items-center flex-col justify-center max-w-2xl">
         <div className="avatar-container">
           <h3 className="">
             <img
