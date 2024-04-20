64bcb46bda97f1fa8b1978b18b7b991c478d243d
Gaurav Porwal <gauravprwl14@gmail.com>
2024-04-20 19:12:51 +0530

    added the about us section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,3 +1,4 @@
+import { Skill } from "@/lib/types";
 import { ABOUT_US } from "@/lib/utils";
 
 export const AboutUs = () => {
@@ -14,8 +15,8 @@ export const AboutUs = () => {
         </div>
 
         <div className="flex flex-col lg:flex-row my-10">
-          <div className="lg:basis-1/3">
-            <div className="font-jost text-8xl flex flex-row mb-12 items-center justify-center font-bold">
+          <div className="lg:basis-1/3 flex flex-col justify-center">
+            <div className="font-jost text-8xl flex flex-row mb-6 lg:mb-12 items-center justify-center font-bold">
               <div className="px-2">{ABOUT_US.experience}</div>
               <div className="-mt-2">+</div>
             </div>
@@ -27,12 +28,62 @@ export const AboutUs = () => {
               </div>
             </div>
           </div>
-          <div className="lg:basis-2/3 bg-blue-500">
-            <div> {ABOUT_US.bio}</div>
-            <div> {ABOUT_US.email}</div>
+          <div className="lg:basis-2/3 my-6 lg:my-2">
+            {/* <div> {ABOUT_US.bio}</div> */}
 
-            <div> card about my domain Web Mobile and backend and AI</div>
+            <div className="text-xl">
+              {ABOUT_US.bio.paragraphs.map((paragraph, index) => (
+                <p key={index} className="py-2">
+                  {paragraph.text.map((part, partIndex) => (
+                    <span
+                      key={partIndex}
+                      style={{
+                        fontWeight: part.highlight ? "normal" : "normal",
+                      }}
+                    >
+                      {part.content}
+                    </span>
+                  ))}
+                </p>
+              ))}
+            </div>
+
+            <div className="text-xl my-5 text-gray-700 font-jost font-bold">
+              {" "}
+              {ABOUT_US.email}
+            </div>
+
+            <div className="flex lg:flex-row flex-col -ml-2 my-2">
+              {" "}
+              {ABOUT_US.skills.map((skill: Skill, index: number) => {
+                return (
+                  <Card
+                    key={index}
+                    icon={skill.icon}
+                    title={skill.title}
+                    description={skill.description}
+                  />
+                );
+              })}{" "}
+            </div>
           </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ icon, title, description }: Skill) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden mb-6 w-full pl-6">
+      <div className="relative text-center bg-white rounded-lg shadow-lg w-full px-5 py-10 h-full clear ">
+        <div className="flex items-center justify-center">
+          <img src={icon} alt="Skill Icon" className="w-12 h-12" />
+        </div>
+        <div className=" mt-5 text-2xl font-semibold"> {title}</div>
+        <div className="text-xl my-5 text-gray-700 font-jost text-center">
+          {" "}
+          <h3>{description}</h3>
         </div>
       </div>
     </div>
