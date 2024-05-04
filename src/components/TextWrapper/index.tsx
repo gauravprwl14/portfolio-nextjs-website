6055413646114b41d4bdf0421e24cabd797786da
Gaurav Porwal <gauravprwl14@gmail.com>
2024-05-05 00:31:35 +0530

    added the text wrapper comp

---
+++ src/components/TextWrapper/index.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+
+export interface TextWrapperProps {
+  text: string;
+  lineClamp?: number;
+}
+
+export const TextWrapper: React.FC<TextWrapperProps> = ({
+  text,
+  lineClamp = 2,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isTruncatable, setIsTruncatable] = useState(false);
+  const textRef = useRef<HTMLParagraphElement>(null);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  useEffect(() => {
+    // Check if textRef is not null
+    if (textRef.current) {
+      const lineHeight = parseFloat(
+        window.getComputedStyle(textRef.current).lineHeight
+      );
+      if (textRef.current.scrollHeight > lineHeight * lineClamp) {
+        setIsTruncatable(true);
+      }
+    }
+  }, [text, lineClamp]);
+
+  return (
+    <div className="w-full">
+      <div className={`relative ${isExpanded ? "" : "h-14"}`}>
+        <p
+          ref={textRef}
+          className={`overflow-hidden overflow-ellipsis ${
+            isExpanded ? "" : "line-clamp-2"
+          }`}
+        >
+          {text}
+        </p>
+        {!isExpanded && isTruncatable && (
+          <div className="absolute bottom-0 right-0 bg-white px-2 py-1">
+            <button
+              className="text-blue-500 hover:text-blue-700"
+              onClick={toggleExpand}
+            >
+              Show more
+            </button>
+          </div>
+        )}
+      </div>
+      {isExpanded && (
+        <button
+          className="text-blue-500 hover:text-blue-700"
+          onClick={toggleExpand}
+        >
+          Show less
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default TextWrapper;
